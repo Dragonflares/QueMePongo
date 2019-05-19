be61920d7fa967f523a71eaef5cb03f9133a335b
Giuliana Vetere <giulianavetere98@gmail.com>
2019-05-19 19:20:37 -0300

    falta terminar test

--- src/test/java/TestDeDominio/TestUsuarios.java
+++ src/test/java/TestDeDominio/TestUsuarios.java
@@ -13,6 +13,7 @@ import Dominio.TipoPrenda;
 import Dominio.Usuario;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 
@@ -28,24 +29,37 @@ public class TestUsuarios {
 	@Before
 	public void init() throws Exception {
 
-		Prenda zapatillasNegras = new Prenda(TipoPrenda.ZAPATILLAS, Color.NEGRO, Color.GRIS, Material.CUERO);
-		Prenda cinturonNaranja = new Prenda(TipoPrenda.CINTURON, Color.NARANJA, Color.NEGRO, Material.CUERO);
-		Prenda pantalonAzul = new Prenda(TipoPrenda.PANTALON, Color.AZUL, Color.AMARILLO, Material.JEAN);
-		Prenda remeraAzul = new Prenda(TipoPrenda.REMERA, Color.AZUL, Color.AMARILLO, Material.ALGODON);
-		remeraRoja = new Prenda(TipoPrenda.REMERA, Color.ROJO, Color.AMARILLO, Material.ALGODON);
+		List<Material> materialesNoCompatiblesZapas = new ArrayList<>();
+		materialesNoCompatiblesZapas.add(Material.LYCRA);
+		materialesNoCompatiblesZapas.add(Material.GABARDINA);
+
+		List<Material> materialesNoCompatiblesCin = new ArrayList<>();
+		materialesNoCompatiblesCin.add(Material.LANA);
+		materialesNoCompatiblesCin.add(Material.GABARDINA);
 		
-		ArrayList<Prenda> prendasSuperiores = new ArrayList<Prenda>();
-		prendasSuperiores.add(remeraRoja);
-		prendasSuperiores.add(remeraAzul);
+		List<Material> materialesNoCompatiblesPant = new ArrayList<>();
+		materialesNoCompatiblesPant.add(Material.PLASTICO);
+		materialesNoCompatiblesPant.add(Material.GOMA);
 		
-		ArrayList<Prenda> prendasInferiores = new ArrayList<Prenda>();
-		prendasInferiores.add(pantalonAzul);
+		List<Material> materialesNoCompatiblesRem = new ArrayList<>();
+		materialesNoCompatiblesRem.add(Material.CUERO);
+		materialesNoCompatiblesRem.add(Material.GABARDINA);
 		
-		ArrayList<Prenda> calzados = new ArrayList<Prenda>();
-		calzados.add(zapatillasNegras);
 		
-		ArrayList<Prenda> accesorios = new ArrayList<Prenda>();
-		accesorios.add(cinturonNaranja);
+		Prenda zapatillasNegras = new Prenda(new TipoPrenda(Categoria.CALZADO, materialesNoCompatiblesZapas), Color.NEGRO, Color.GRIS, Material.CUERO);
+		Prenda cinturonNaranja = new Prenda(new TipoPrenda(Categoria.ACCESORIOS, materialesNoCompatiblesCin), Color.NARANJA, Color.NEGRO, Material.CUERO);
+		Prenda pantalonAzul = new Prenda(new TipoPrenda(Categoria.PARTE_INFERIOR, materialesNoCompatiblesPant), Color.AZUL, Color.AMARILLO, Material.JEAN);
+		Prenda remeraAzul = new Prenda(new TipoPrenda(Categoria.PARTE_SUPERIOR, materialesNoCompatiblesRem), Color.AZUL, Color.AMARILLO, Material.ALGODON);
+		Prenda remeraRoja = new Prenda(new TipoPrenda(Categoria.PARTE_SUPERIOR, materialesNoCompatiblesRem), Color.ROJO, Color.AMARILLO, Material.ALGODON);
+		
+		
+		ArrayList<Prenda> prendas = new ArrayList<Prenda>();
+		prendas.add(cinturonNaranja);
+		prendas.add(remeraRoja);
+		prendas.add(remeraAzul);
+		prendas.add(pantalonAzul);
+		prendas.add(zapatillasNegras);
+		prendas.add(cinturonNaranja);
 		
 		ArrayList<Prenda> atuendo1 = new ArrayList<Prenda>();
 		atuendo1.add(remeraRoja);
@@ -55,8 +69,8 @@ public class TestUsuarios {
 
 		atuendo = new Atuendo(atuendo1);
 		
-		guardarropas1 = new Guardarropa(prendasSuperiores, prendasInferiores, calzados, accesorios);
-		guardarropas2 = new Guardarropa(prendasSuperiores, prendasInferiores, calzados, accesorios);
+		guardarropas1 = new Guardarropa(prendas);
+		guardarropas2 = new Guardarropa(prendas);
 		
 		julieta = new Usuario("giu", guardarropas1);
 	}
@@ -68,12 +82,12 @@ public class TestUsuarios {
 	
 	@Test
 	public void laPrimerPrendaDelPrimerAtuendoEsUnaRemeraRoja() {
-		Assert.assertEquals(atuendo.getPrendas().get(0), guardarropas1.generarRecomendaciones().get(0).getPrendas().get(0));
+		Assert.assertEquals(atuendo.getPrendas().get(0), guardarropas1.generarRecomendacion().get(0).getPrendas().get(0));
 	}
 	
 	@Test
 	public void laSegundaPrendaDelPrimerAtuendoEsUnPantalonAzul() {
-		Assert.assertEquals(atuendo.getPrendas().get(1), guardarropas1.generarRecomendaciones().get(0).getPrendas().get(1));
+		Assert.assertEquals(atuendo.getPrendas().get(1), guardarropas1.generarRecomendacion().get(0).getPrendas().get(1));
 	}
 	
 	@Test
