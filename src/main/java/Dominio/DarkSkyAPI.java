4ddf8e5430ebec9a83bdaf14a989dfa10d1fa225
Dragonflares <ivanlose@hotmail.com>
2019-08-05 22:30:45 -0300

    Parece que esto debería funcionar

--- src/main/java/Dominio/DarkSkyAPI.java
+++ src/main/java/Dominio/DarkSkyAPI.java
@@ -18,22 +18,29 @@ public class DarkSkyAPI {
 	{
 		DsClient client = new DsClient("9ee9579e309df4f7c0806b5e60b90e71");
 		DsTimeMachineRequest request = DsTimeMachineRequest.builder()
-		        .latitude("-34.6157437")
+				.latitude("-34.6157437")
 				.longitude("-58.5733854")				
 				.unit(DsUnit.SI)
 				.time(ZonedDateTime.now().minusDays(-day).toEpochSecond())
 				.build();
 		DsResponse response = client.sendTimeMachineRequest(request);
-		for (DsDataPoint dataPoint : response.hourly().data()) {			
-		    ZoneId zoneId = ZoneId.of(response.timezone());
-		    Instant instant = Instant.ofEpochSecond(dataPoint.time());
-		    ZonedDateTime time = ZonedDateTime.ofInstant(instant, zoneId);
 
-		    System.out.print(time);
-		    System.out.print(": ");
-		    System.out.println(dataPoint);            
-		}
 		float temp = 0;
+		int iteration = 1;
+		for (DsDataPoint dataPoint : response.hourly().data()) 
+		{			
+			ZoneId zoneId = ZoneId.of(response.timezone());
+			Instant instant = Instant.ofEpochSecond(dataPoint.time());
+			ZonedDateTime time = ZonedDateTime.ofInstant(instant, zoneId);
+			if(iteration == day && iteration == 10)
+			{
+				temp = (Float.parseFloat(dataPoint.temperatureMax().toString()) +
+						Float.parseFloat(dataPoint.temperatureMin().toString()))
+						/2;
+			}
+			else
+				iteration++;          
+		}
 		return temp;
 	}
 }
