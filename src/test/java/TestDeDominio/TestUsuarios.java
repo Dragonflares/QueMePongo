4e9e59d367847521563ca095ec533271847c7786
Dragonflares <ivanlose@hotmail.com>
2019-08-26 22:27:54 -0300

    Update TestUsuario
    Agrego 6 tests de Usuario

--- src/test/java/TestDeDominio/TestUsuarios.java
+++ src/test/java/TestDeDominio/TestUsuarios.java
@@ -1,7 +1,12 @@
 package TestDeDominio;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.Calendar;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,16 +32,17 @@ public class TestUsuarios {
 	private Prenda camperaFinita;
 	private Prenda pantalonLargo;
 	private Prenda pantalonCorto;
+	private Calendar date;
 	
 	@Before
 	public void init() {
 		atuendoMedio = new Atuendo();
 		atuendoPocoAbrigado = new Atuendo();
 		
-		usuario1 = mock(Usuario.class);
-		
-		when(usuario1.getOffsetSuperior()).thenReturn(0);
-		when(usuario1.getOffsetInferior()).thenReturn(0);
+		usuario1 = new Usuario("Dragonflares", "lorddragonflares@gmail.com", "+54 9 1133606009");
+		usuario1.modificarOffSetSuperior(1);
+		date = Calendar.getInstance();
+		date.add(Calendar.DATE, -2);
 		
 		remeraMangaCorta = mock(Prenda.class);
 		camperaFinita = mock(Prenda.class);
@@ -67,8 +73,32 @@ public class TestUsuarios {
 	}
 	
 	@Test
-	public void test() {
-		
+	public void tieneMásCalorArriba() {
+		assertTrue(0 < usuario1.getOffsetSuperior());
 	}
 	
+	@Test
+	public void noLePareceMalEnLasPiernas() {
+		assertTrue(0 == usuario1.getOffsetInferior());
+	}
+	
+	@Test
+	public void noTieneEventosSinNotificar() {
+		assertFalse(usuario1.tieneEventoSinNotificar());
+	}
+	
+	@Test
+	public void noTieneEventosOcurridos() {
+		assertFalse(usuario1.tieneEventosOcurridos(date));
+	}
+	
+	@Test
+	public void noTieneSugerenciasPorCalificar() {
+		assertTrue(usuario1.getSugerenciasQueFaltanCalificar().isEmpty());
+	}
+	
+	@Test
+	public void elCelularEstaBien() {
+		assertEquals("+54 9 1133606009", usuario1.getNumeroCelular());
+	}
 }
