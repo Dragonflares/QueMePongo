977d20f0fcfc64785596f4d2d49fa81c504b71dc
Dragonflares <ivanlose@hotmail.com>
2019-09-12 20:42:52 -0300

    Update generarRecomendación
    Ahora todos los métodos que lo conforman son "private"
    Ahora existen capas de calzado(por medias) y capas de prendasInferiores (por poder usar calzas)
    Después va a haber que modificar para que exista una variacion superior de temperatura para calzado y para parte inferior porque el calor es menos importante ahí.
    También faltaría testear todo eso, pero bueno.

--- src/main/java/Dominio/WardrobeClasses/Guardarropa.java
+++ src/main/java/Dominio/WardrobeClasses/Guardarropa.java
@@ -31,7 +31,7 @@ public class Guardarropa {
 	{
 		return prendasDisponibles.size();
 	}
-	
+
 	public List<Usuario> getUsuariosConAcceso(){
 		return this.usuariosConAcceso;
 	}
@@ -68,10 +68,10 @@ public class Guardarropa {
 	private double pedirTemperatura(Calendar fecha, int hora) throws IOException {
 		return GestorClimatico.getInstance().obtenerTemperatura(fecha, hora);
 	}
-	public Atuendo generarRecomendacion(Evento evento, Usuario creador) throws Exception
-	{
+
+	public Atuendo generarRecomendacion(Evento evento,
+			Usuario creador) throws Exception {
 		Atuendo atuendo = null;
-		ArrayList<Prenda> prendasDeAtuendo = new ArrayList<Prenda>();
 		double temperatura = pedirTemperatura(evento.getFecha(), evento.getFecha().get(Calendar.HOUR_OF_DAY));
 		if(temperatura >= Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase")) - creador.getOffsetSuperior())
 		{
@@ -79,61 +79,44 @@ public class Guardarropa {
 		}
 		else
 		{
-			List<Prenda> prendasSuperiores = obtenerParteSuperior(evento, temperatura, 0, creador);
-			Prenda prendaInferior = obtenerParteInferior(evento, temperatura, creador);
-			Prenda calzado = obtenerCalzado(evento, temperatura);
-			if(prendasSuperiores.isEmpty() || prendaInferior.equals(null) || calzado.equals(null))
-			{
-				atuendo = null;
-			}
-			else {
-				prendasDeAtuendo.addAll(prendasSuperiores);
-				prendasDeAtuendo.add(prendaInferior);
-				prendasDeAtuendo.add(calzado);
-				atuendo = new Atuendo(prendasDeAtuendo);
-			}
+			atuendo = new Atuendo(generarAtuendoRandom(evento, temperatura, creador));
 		}
-		
+
+
 		return atuendo;
 	}
 
-	public List<Prenda> obtenerParteSuperior(Evento evento, double temperatura, int intento, Usuario creador) throws Exception{
-		List<Prenda> prendasSuperiores = new ArrayList<Prenda>();
-		Random rand = new Random();
-		int calorActual;
-		int a = 0;
-		do{
-			final int b = a;
-
-			List<Prenda> prendasSuperioresPosibles = getPrendasSuperioresDisponibles()
-					.stream()
-					.filter(p -> p.getTipoRopa().esDeEsaCapa(b))
-					.collect(Collectors.toList());
-			int limitSup = prendasSuperioresPosibles.size();
-			if(limitSup != 0) {
-				int indexSuperior = rand.nextInt(limitSup);
-				Prenda prendaSuperior = null;
-				int c = 0;
-				while(c != Integer.parseInt(Property.getSpecifiedProperty("CantIntentos"))){
-					final Prenda nuevaPrendaSuperior = prendasSuperioresPosibles.get(indexSuperior);
-					if(prendasSuperiores.stream().anyMatch(p -> p.getTipoRopa().compararNombres(nuevaPrendaSuperior.getTipoRopa().getNombre())))
-					{	
-						c++;
-						prendaSuperior = nuevaPrendaSuperior;
-					}
-					else
-						break;
-				}
-				prendasSuperiores.add(prendaSuperior);
-				calorActual = creador.getOffsetSuperior() + (prendasSuperiores.stream()
-						.mapToInt(p -> p.getTipoRopa().getNivelAbrigo()).sum());
-				a++;
-			}
-			else {
-				throw new Exception("No hay suficientes prendas para generar el atuendo");
-			}
+	private ArrayList<Prenda> generarAtuendoRandom(Evento evento,
+			double temperatura, Usuario creador) throws Exception {
+		ArrayList<Prenda> prendasDeAtuendo = new ArrayList<Prenda>();
+		List<Prenda> prendasSuperiores = 
+				obtenerParteSuperior(evento, temperatura, 0, creador);
+		List<Prenda> prendaInferior = obtenerParteInferior(evento, temperatura, creador, 0);
+		List<Prenda> calzado = obtenerCalzado(evento, temperatura, creador , 0);
+		if(prendasSuperiores.isEmpty() || prendaInferior.equals(null) || calzado.equals(null))
+		{
+			return null;
 		}
-		while(calorActual < (Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase")) - temperatura - 3) && a < 6);
+		else {
+			prendasDeAtuendo.addAll(prendasSuperiores);
+			prendasDeAtuendo.addAll(prendaInferior);
+			prendasDeAtuendo.addAll(calzado);
+		}
+		return prendasDeAtuendo;
+	}
+
+	private List<Prenda> obtenerParteSuperior(Evento evento,
+			double temperatura, int intento, Usuario creador) throws Exception {
+		List<Prenda> prendasSuperiores = new ArrayList<Prenda>();
+		int calorActual;
+		int capa = 0;
+		do{
+			prendasSuperiores.add(darPrendaSuperiorDeCapa(capa, prendasSuperiores));
+			calorActual = creador.getOffsetSuperior() + (prendasSuperiores.stream()
+					.mapToInt(p -> p.getTipoRopa().getNivelAbrigo()).sum());
+			capa++;
+		}
+		while(calorActual < (Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase")) - temperatura - 3) && capa < 6);
 		if(calorActual > (Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase"))
 				- temperatura 
 				+ Integer.parseInt(Property.getSpecifiedProperty("VariacionTemperatura"))))
@@ -150,28 +133,101 @@ public class Guardarropa {
 			return prendasSuperiores;
 		}
 	}
-	public Prenda obtenerParteInferior(Evento evento, double temperatura, Usuario creador) throws Exception {
-		Prenda prendaInferior;
 
-		if(temperatura > (Integer.parseInt(Property.getSpecifiedProperty("TemperaturaInferiorBase")) - creador.getOffsetInferior())) {
-			prendaInferior = obtenerPrendaInf(0);
+	private Prenda darPrendaSuperiorDeCapa(Integer capa,
+			List<Prenda> prendasSuperiores ) throws Exception {
+		Random rand = new Random();
+		List<Prenda> prendasSuperioresPosibles = getPrendasSuperioresDisponibles()
+				.stream()
+				.filter(p -> p.getTipoRopa().esDeEsaCapa(capa))
+				.collect(Collectors.toList());
+		int limitSup = prendasSuperioresPosibles.size();
+		if(limitSup != 0) {
+			Prenda prendaSuperior = null;
+			int intento = 0;
+			while(intento != Integer.parseInt(Property.getSpecifiedProperty("CantIntentos")))
+			{
+				int indexSuperior = rand.nextInt(limitSup);
+				final Prenda nuevaPrendaSuperior = 
+						prendasSuperioresPosibles.get(indexSuperior);
+				if(!prendasSuperiores.stream()
+						.anyMatch(p -> p.getTipoRopa()
+								.compararNombres(nuevaPrendaSuperior
+										.getTipoRopa().getNombre()))) 
+				{	
+					prendaSuperior = nuevaPrendaSuperior;
+					break;
+				}
+				else
+					intento++;
+			}
+			return prendaSuperior;
 		}
 		else {
-			prendaInferior = obtenerPrendaInf(1);
+			throw new Exception("No hay suficientes prendas para generar el atuendo");
 		}
-		return prendaInferior;
 	}
-	public Prenda obtenerPrendaInf(int nivelAbrigo) throws Exception {
-		Prenda prendaInferior;
+
+	private List<Prenda> obtenerParteInferior(Evento evento,
+			double temperatura, Usuario creador, Integer intento) throws Exception {
+		List<Prenda> prendasInferiores = new ArrayList<Prenda>();
+		Integer calorActual;
+		Integer capa = 1;
+		do{
+			prendasInferiores.add(obtenerPrendaInf(capa, prendasInferiores));
+			calorActual = creador.getOffsetSuperior() + (prendasInferiores.stream()
+					.mapToInt(p -> p.getTipoRopa().getNivelAbrigo()).sum());
+			capa++;
+		}
+		while(calorActual < 
+				(Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase"))
+						- temperatura - Integer.parseInt(Property.getSpecifiedProperty("VariacionTemperatura")))
+				&& capa > -1);
+		if(calorActual > (Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase"))
+				- temperatura 
+				+ Integer.parseInt(Property.getSpecifiedProperty("VariacionTemperatura"))))
+		{
+			if(intento ==  Integer.parseInt(Property.getSpecifiedProperty("CantIntentos"))) {
+				return null;
+			}
+			else {
+				intento++;
+				return prendasInferiores = 
+						obtenerParteInferior(evento, temperatura, creador, intento);
+			}
+		}
+		else {
+			return prendasInferiores;
+		}
+	}
+
+	private Prenda obtenerPrendaInf(Integer capa,
+			List<Prenda> prendasInfActuales) throws Exception {
+		Prenda prendaInferior = null;
 		Random rand = new Random();
-		List<Prenda> prendasInferioresPosibles = getPrendasSuperioresDisponibles()
+		List<Prenda> prendasInferioresPosibles = getPrendasInferioresDisponibles()
 				.stream()
-				.filter(p -> p.getTipoRopa().getNivelAbrigo() == nivelAbrigo)
+				.filter(p -> p.getTipoRopa().esDeEsaCapa(capa))
 				.collect(Collectors.toList());
 		int limitInf = prendasInferioresPosibles.size();
 		if(limitInf > 0) {
-			int indexInferior = rand.nextInt(limitInf);
-			prendaInferior = prendasInferioresPosibles.get(indexInferior);
+			Integer intento = 0;
+			while(intento != 
+					Integer.parseInt(Property.getSpecifiedProperty("CantIntentos")))
+			{
+				int indexInferior = rand.nextInt(limitInf);
+				Prenda nuevaPrendaInferior = prendasInferioresPosibles.get(indexInferior);
+				if(!prendasInfActuales.stream()
+						.anyMatch(p -> p.getTipoRopa()
+								.compararNombres(nuevaPrendaInferior
+										.getTipoRopa().getNombre()))) 
+				{	
+					prendaInferior = nuevaPrendaInferior;
+					break;
+				}
+				else
+					intento++;
+			}
 		}
 		else {
 			throw new Exception("No hay suficientes prendas inferiores "
@@ -179,20 +235,75 @@ public class Guardarropa {
 		}
 		return prendaInferior;
 	}
-	public Prenda obtenerCalzado(Evento evento, double temperatura) throws Exception {
-		Prenda calzado;
-		Random rand = new Random();
-		int limitCalzados = getCalzadosDisponibles().size();
-		if(limitCalzados != 0) {
-			int indexCalzados = rand.nextInt(limitCalzados);
-			calzado = getCalzadosDisponibles().get(indexCalzados);
+
+	private List<Prenda> obtenerCalzado(Evento evento,
+			double temperatura, Usuario creador, Integer intento) throws Exception {
+		List<Prenda> calzado = new ArrayList<Prenda>();
+		Integer calorActual;
+		Integer capa = 1;
+		do{
+			calzado.add(obtenerParteDeCalzado(capa, calzado));
+			calorActual = creador.getOffsetSuperior() + (calzado.stream()
+					.mapToInt(p -> p.getTipoRopa().getNivelAbrigo()).sum());
+			capa++;
+		}
+		while(calorActual < 
+				(Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase"))
+						- temperatura - Integer.parseInt(Property.getSpecifiedProperty("VariacionTemperatura")))
+				&& capa > -1);
+		if(calorActual > (Integer.parseInt(Property.getSpecifiedProperty("TemperaturaBase"))
+				- temperatura 
+				+ Integer.parseInt(Property.getSpecifiedProperty("VariacionTemperatura"))))
+		{
+			if(intento ==  Integer.parseInt(Property.getSpecifiedProperty("CantIntentos"))) {
+				return null;
+			}
+			else {
+				intento++;
+				return calzado = 
+						obtenerCalzado(evento, temperatura, creador, intento);
+			}
 		}
 		else {
-			throw new Exception("No hay suficientes calzados "
+			return calzado;
+		}
+	}
+
+	private Prenda obtenerParteDeCalzado(Integer capa,
+			List<Prenda> prendasInfActuales) throws Exception {
+		Prenda prendaDeCalzado = null;
+		Random rand = new Random();
+		List<Prenda> calzadosDisponibles = getCalzadosDisponibles()
+				.stream()
+				.filter(p -> p.getTipoRopa().esDeEsaCapa(capa))
+				.collect(Collectors.toList());
+		int limitInf = calzadosDisponibles.size();
+		if(limitInf > 0) {
+			Integer intento = 0;
+			while(intento != 
+					Integer.parseInt(Property.getSpecifiedProperty("CantIntentos")))
+			{
+				int indexInferior = rand.nextInt(limitInf);
+				Prenda nuevoCalzado = calzadosDisponibles.get(indexInferior);
+				if(!prendasInfActuales.stream()
+						.anyMatch(p -> p.getTipoRopa()
+								.compararNombres(nuevoCalzado
+										.getTipoRopa().getNombre()))) 
+				{	
+					prendaDeCalzado = nuevoCalzado;
+					break;
+				}
+				else
+					intento++;
+			}
+		}
+		else {
+			throw new Exception("No hay suficientes prendas inferiores "
 					+ "para crear el atuendo");
 		}
-		return calzado;
+		return prendaDeCalzado;
 	}
+	
 	//	public List<Prenda> obtenerAccesorios(Evento evento) throws IOException{
 	//		List<Prenda> accesorios = new ArrayList<Prenda>();
 	//		int limitAccesorios = getAccesoriosDisponibles().size();
