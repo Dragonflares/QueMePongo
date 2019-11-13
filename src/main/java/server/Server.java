49b1bb38b89aec759bf3dfb0a35536ce7483dfcd
MelisaRodirguez <melisarodrig@hotmail.com>
2019-11-13 11:29:57 -0300

    elimino archivos hbs sin utilizar y cambio insertarUsuariosParaProbar()

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -37,42 +37,45 @@ public class Server {
 	
 	public static void insertarUsuariosParaProbar() throws ProcessingDataFailedException, Exception 
 	{
-		Usuario usuario1 = new Usuario("usuario123", "asd123");
-		Usuario usuario2 = new Usuario("lala", "qwe123");
-		usuario2.setTipoDeCuenta(new Premium());
-		Guardarropa guardarropa1 = new Guardarropa(usuario1, Estilo.CASUAL);
-		Guardarropa guardarropa2 = new Guardarropa(usuario1, Estilo.ELEGANTE);
-		Calendar dentroDeUnaSemana = Calendar.getInstance();
-		dentroDeUnaSemana.add(Calendar.DATE, 7); 
-		Evento evento1 = new Evento("Cumpleaños", Calendar.getInstance(), "avenida 123", Estilo.ELEGANTE_SPORT, null, new Media());
-		Evento evento2 = new Evento("Casamiento", dentroDeUnaSemana, "calle 13", Estilo.ELEGANTE, null, new Alta());
-		Evento evento3 = new Evento("Fiesta", Calendar.getInstance(), "calle falsa 123", Estilo.CASUAL, null, new Baja());
-		Evento evento4 = new Evento("Bautismo", dentroDeUnaSemana, "iglesa 123", Estilo.FORMAL, null, new Media());
-		usuario1.agregarEvento(evento1);
-		usuario1.agregarEvento(evento2);
-		usuario2.agregarEvento(evento3);
-		usuario2.agregarEvento(evento4);
-		
-		Prenda remeraMangaLargaRojaDeAlgodon = new Prenda.PrendaBuilder()
-				.material("Algodon")
-				.nombrePrenda("remeraMangaLargaRojaDeAlgodon")
-				.setearColores("Rojo", "Negro")
-				.tipoRopa("Remera manga larga")
-				.build();
-		Prenda pantalonLargoNegroDeLycra = new Prenda.PrendaBuilder()
-				.material("Lycra")
-				.nombrePrenda("pantalonLargoNegroDeLycra")
-				.setearColores("Negro", "Azul")
-				.tipoRopa("Pantalon largo")
-				.build();
-		
-		
-		usuario1.agregarGuardarropa(guardarropa1);
-		usuario2.agregarGuardarropa(guardarropa2);
-		usuario1.agregarPrendaAGuardarropa(guardarropa1, pantalonLargoNegroDeLycra);
-		usuario2.agregarPrendaAGuardarropa(guardarropa2, remeraMangaLargaRojaDeAlgodon);
-		FactoryRepositorioUsuario.get().agregar(usuario1);
-		FactoryRepositorioUsuario.get().agregar(usuario2);
+		if(!FactoryRepositorioUsuario.get().existeUsuario("usuario123", "asd123"))
+		{
+			Usuario usuario1 = new Usuario("usuario123", "asd123");
+			Usuario usuario2 = new Usuario("lala", "qwe123");
+			usuario2.setTipoDeCuenta(new Premium());
+			Guardarropa guardarropa1 = new Guardarropa(usuario1, Estilo.CASUAL);
+			Guardarropa guardarropa2 = new Guardarropa(usuario1, Estilo.ELEGANTE);
+			Calendar dentroDeUnaSemana = Calendar.getInstance();
+			dentroDeUnaSemana.add(Calendar.DATE, 7); 
+			Evento evento1 = new Evento("Cumpleaños", Calendar.getInstance(), "avenida 123", Estilo.ELEGANTE_SPORT, null, new Media());
+			Evento evento2 = new Evento("Casamiento", dentroDeUnaSemana, "calle 13", Estilo.ELEGANTE, null, new Alta());
+			Evento evento3 = new Evento("Fiesta", Calendar.getInstance(), "calle falsa 123", Estilo.CASUAL, null, new Baja());
+			Evento evento4 = new Evento("Bautismo", dentroDeUnaSemana, "iglesa 123", Estilo.FORMAL, null, new Media());
+			usuario1.agregarEvento(evento1);
+			usuario1.agregarEvento(evento2);
+			usuario2.agregarEvento(evento3);
+			usuario2.agregarEvento(evento4);
+			
+			Prenda remeraMangaLargaRojaDeAlgodon = new Prenda.PrendaBuilder()
+					.material("Algodon")
+					.nombrePrenda("remeraMangaLargaRojaDeAlgodon")
+					.setearColores("Rojo", "Negro")
+					.tipoRopa("Remera manga larga")
+					.build();
+			Prenda pantalonLargoNegroDeLycra = new Prenda.PrendaBuilder()
+					.material("Lycra")
+					.nombrePrenda("pantalonLargoNegroDeLycra")
+					.setearColores("Negro", "Azul")
+					.tipoRopa("Pantalon largo")
+					.build();
+			
+			
+			usuario1.agregarGuardarropa(guardarropa1);
+			usuario2.agregarGuardarropa(guardarropa2);
+			usuario1.agregarPrendaAGuardarropa(guardarropa1, pantalonLargoNegroDeLycra);
+			usuario2.agregarPrendaAGuardarropa(guardarropa2, remeraMangaLargaRojaDeAlgodon);
+			FactoryRepositorioUsuario.get().agregar(usuario1);
+			FactoryRepositorioUsuario.get().agregar(usuario2);
+		}
 	}
 
 }
