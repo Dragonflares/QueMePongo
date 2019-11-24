f8f7af728e24f492c79690c01ee118ab629f591c
Martin Olivera <jm98olivera@gmail.com>
2019-11-24 18:02:27 -0300

    prenda sale mal (muy)

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -47,8 +47,8 @@ public class Server {
 	
 	public static void insertarUsuariosParaProbar() throws ProcessingDataFailedException, Exception 
 	{
-		if(!FactoryRepositorioUsuario.get().existeUsuario("usuario123", "asd123"))
-		{
+		//if(!FactoryRepositorioUsuario.get().existeUsuario("usuario123", "asd123"))
+		//{
 			Usuario usuario1 = new Usuario("usuario123", "asd123");
 			Usuario usuario2 = new Usuario("lala", "qwe123");
 			
@@ -150,7 +150,7 @@ public class Server {
 			usuario2.agregarPrendaAGuardarropa(guardarropa2, remeraMangaLargaRojaDeAlgodon);
 			FactoryRepositorioUsuario.get().agregar(usuario1);
 			FactoryRepositorioUsuario.get().agregar(usuario2);
-		}
+		//}
 	}
 
 }
